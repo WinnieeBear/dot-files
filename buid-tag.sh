d4140cb6441431187ac21a4b3f706eb6f92e1f9a
Sean Kuo <gcf1129@gmail.com>
2021-05-29 22:16:00 +0800

    add ctags shell

---
+++ buid-tag.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ctags -R --c++-kinds=+p+l+x+c+d+e+f+g+m+n+s+t+u+v --fields=+liaS --extra=+q --java-kinds=+l  --python-kinds=-i
+CSCOPE_DIR="$PWD/cscope"
+ 
+if [ ! -d "$CSCOPE_DIR" ]; then
+mkdir "$CSCOPE_DIR"
+fi
+ 
+echo "Finding files ..."
+find "$PWD" -name '*.[ch]' \
+-o -name '*.cpp' \
+-o -name '*.cc' \
+-o -name '*.hpp' \
+-o -name '*.py' \
+-o -name '*.java' \
+> "$CSCOPE_DIR/cscope.files"
+ 
+echo "Adding files to cscope db: $PWD/cscope.db ..."
+cscope -b -i "$CSCOPE_DIR/cscope.files"
+ 
+export CSCOPE_DB="$PWD/cscope.out"
+echo "Exported CSCOPE_DB to: '$CSCOPE_DB'"
